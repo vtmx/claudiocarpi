c2d24bd987a9a58eb813e16ec187824f677cadde
Vitor Melo <vtmx22@gmail.com>
2017-11-03 04:01:02 -0200

    Fix single work normal size.

--- single-work.php
+++ single-work.php
@@ -13,7 +13,8 @@
 
 				<?php // Get work size ?>
 				<?php $imageSize = get_field('work-size', $image[id]); ?>
-				<?php $size = $image['url']; ?>
+				
+				<!-- <?php $size = $image['url']; ?>
 
 				<?php if ( $imageSize == 4 ): ?>
 					<?php $col = '4'; ?>
@@ -23,6 +24,20 @@
 					<?php $col = '2'; ?>
 				<?php else: ?>
 					<?php $col = '1'; ?>
+				<?php endif; ?> -->
+
+				<?php if ( $imageSize == 4 ): ?>
+					<?php $col = '4'; ?>
+					<?php $size = $image['sizes']['work-single-4']; ?>
+				<?php elseif ( $imageSize == 3 ): ?>
+					<?php $col = '3'; ?>
+					<?php $size = $image['sizes']['work-single-3']; ?>
+				<?php elseif ( $imageSize == 2 ): ?>
+					<?php $col = '2'; ?>
+					<?php $size = $image['sizes']['work-single-2']; ?>
+				<?php else: ?>
+					<?php $col = '1'; ?>
+					<?php $size = $image['sizes']['work-single-1']; ?>
 				<?php endif; ?>
 
 				<div class="work w<?php echo $col; ?>">
