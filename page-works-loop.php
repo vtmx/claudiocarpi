6193caf2a344c087e431ad5f329511d69952bdb9
Vitor Melo <vtmx22@gmail.com>
2017-11-04 01:30:10 -0200

    Page works max resolution.

--- page-works-loop.php
+++ page-works-loop.php
@@ -14,19 +14,21 @@
 			<?php // If thumb image ?>
 			<?php if ( $imageThumb ): ?>
 
+				<?php //Max Resolution ?>
+
+				<?php $size = $imageThumb['url']; ?>
+
 				<?php if ( $imageSize == 3 ): ?>
 					<?php $col = '3'; ?>
-					<?php $size = $imageThumb['sizes']['work-gallery-3']; ?>
 				<?php elseif ( $imageSize == 2 ): ?>
 					<?php $col = '2'; ?>
-					<?php $size = $imageThumb['sizes']['work-gallery-2']; ?>
 				<?php else: ?>
 					<?php $col = '1'; ?>
-					<?php $size = $imageThumb['sizes']['work-gallery-1']; ?>
 				<?php endif; ?>
 
+
 					<a class="work image w<?php echo $col; ?> <?php echo $post->post_name; ?>" href="<?php the_permalink(); ?>">
-						<img src="<?php echo $size ?>" alt="<?php the_title(); ?>">
+						<img src="<?php echo $size; ?>" alt="<?php the_title(); ?>">
 						<h2><?php the_title(); ?></h2>
 					</a>
 
