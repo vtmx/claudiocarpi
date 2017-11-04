cc2395b26f81a6e948f944aee2bbf9e2af86215d
Vitor Melo <vtmx22@gmail.com>
2017-11-04 12:37:02 -0200

    Change function image size.

--- functions.php
+++ functions.php
@@ -14,13 +14,13 @@
 	// -----------------------------------------------------------------------------
 
 	// Work Gallery
-	add_image_size( 'work-gallery-1', 370, 504, true );
-	add_image_size( 'work-gallery-2', 760, 504, true );
-	add_image_size( 'work-gallery-3', 1150, 504, true );
-
-	// Work Single
-	add_image_size( 'work-single-1', 370, 9999, false );
-	add_image_size( 'work-single-2', 565, 9999, false );
-	add_image_size( 'work-single-3', 760, 9999, false );
-	add_image_size( 'work-single-4', 1150, 9999, false );
+	// add_image_size( 'work-gallery-1', 370, 504, true );
+	// add_image_size( 'work-gallery-2', 760, 504, true );
+	// add_image_size( 'work-gallery-3', 1150, 504, true );
+	//
+	// // Work Single
+	// add_image_size( 'work-single-1', 370, 9999, false );
+	// add_image_size( 'work-single-2', 565, 9999, false );
+	// add_image_size( 'work-single-3', 760, 9999, false );
+	// add_image_size( 'work-single-4', 1150, 9999, false );
 ?>
